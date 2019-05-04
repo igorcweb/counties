213c43b0befa7ee15518f3f1c7c1eca5ebb9ce78
Igor Chernyshev <igorchern07@gmail.com>
2019-05-04 17:19:08 -0500

    counties

---
+++ app.js
@@ -0,0 +1,31 @@
+const counties = [];
+
+let $state = 'texas';
+let stateCode = 'TX';
+let stateId = '48';
+let stateCounty = `cb_2015_${$state}_county_20m.geometries`;
+
+const url = `https://raw.githubusercontent.com/igorcweb/topojson/master/countries/us-states/${stateCode}-${stateId}-${$state}-counties.json`;
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    data = data.objects;
+    data = data[`cb_2015_${$state}_county_20m`];
+    data.geometries.forEach(county => {
+      county = county.properties.NAME;
+      counties.push(county);
+    });
+
+    //['cb_2015_minnesota_county/_20m.geometries;
+    // data'].forEach(county => {
+    //   county = county.properties.NAME;
+    //   counties.push(county);
+    // });
+  })
+  .then(() => {
+    counties;
+    console.log(counties.sort());
+  });
