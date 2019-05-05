8924840ed176f574929580166eeb6eca2397050d
Igor Chernyshev <igorchern07@gmail.com>
2019-05-05 18:55:23 -0500

    api inconsistencies fixed

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const counties = [];
 
-let $state = 'texas';
-let stateCode = 'TX';
-let stateId = 48;
+let $state = 'wyoming';
 
-const url = `https://raw.githubusercontent.com/igorcweb/topojson/master/countries/us-states/${stateCode}-${stateId}-${$state}-counties.json`;
+let url =
+  $state !== 'louisiana'
+    ? `https://raw.githubusercontent.com/igorcweb/topojson/master/countries/us-states/${$state}-counties.json`
+    : `https://raw.githubusercontent.com/igorcweb/topojson/master/countries/us-states/louisiana-parishes.json`;
 
 fetch(url)
   .then(response => response.json())
   .then(myJson => {
-    data = myJson.objects[`cb_2015_${$state}_county_20m`].geometries;
+    // let data;
+    if ($state.indexOf('-') !== -1) {
+      $state.replace('-', '_');
+    }
+
+    const data =
+      $state !== 'louisiana'
+        ? myJson.objects[`cb_2015_${$state}_county_20m`].geometries
+        : myJson.objects['cb_2015_louisiana_parish_20m'].geometries;
     data.forEach(county => {
       county = county.properties.NAME;
       counties.push(county);
