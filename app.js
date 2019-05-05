9f566c571865b7236a4cc8fa4cea2ff935f0a49e
Igor Chernyshev <igorchern07@gmail.com>
2019-05-04 23:00:57 -0500

    refactor

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ let stateId = '48';
 const url = `https://raw.githubusercontent.com/igorcweb/topojson/master/countries/us-states/${stateCode}-${stateId}-${$state}-counties.json`;
 
 fetch(url)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(results) {
-    data = results.objects[`cb_2015_${$state}_county_20m`];
+  .then(response => response.json())
+  .then(myJson => {
+    data = myJson.objects;
+    data = data[`cb_2015_${$state}_county_20m`];
     data.geometries.forEach(county => {
       county = county.properties.NAME;
       counties.push(county);
